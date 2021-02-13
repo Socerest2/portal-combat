b35a7209b657f75210304cd09f45e7c64b7530c9
Socerest2 <vincentjp920@outlook.com>
2021-02-13 16:08:27 -0500

    tldr; button go to Player.tscn
    Newest Push Makes The Play Button Go To Player.tscn
    Changes to be committed:
    	modified:   Play.gd

--- Play.gd
+++ Play.gd
@@ -1,20 +1,9 @@
 extends Button
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
+#script for play button lmao
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Play_pressed():
-	get_tree().change_scene("res://main.tscn")
+	get_tree().change_scene("res://Player.tscn")
