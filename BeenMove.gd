5223cbc01a74c759ffad15b8c1acfc3691eb0ac7
Collig0 <43286422+Collig0@users.noreply.github.com>
2021-02-12 19:33:21 -0500

    Fix player a bit and add basic left to right movement

--- BeenMove.gd
+++ BeenMove.gd
@@ -1,16 +1,18 @@
-extends Sprite
+extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var velocity = Vector2.ZERO
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = 4
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = -4
+	else:
+		velocity.x - 0
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = 4
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = -4
+	else:
+		velocity.y = 0
