b731efb4639e933202538fa041ecc650a1a5701a
Collig0 <43286422+Collig0@users.noreply.github.com>
2021-02-16 15:36:49 -0500

    Add analog stick controller support, fix menus, fix scaling, change been to new sprite sheet with up+down movement for later, and more

--- BeenMove.gd
+++ BeenMove.gd
@@ -1,21 +1,19 @@
 extends KinematicBody2D
 
 
-var velocity = Vector2.ZERO #Make a variable for velocity so it can be changed later.
+const ACCELERATION = 400
+const MAX_SPEED = 100
+const FRICTION = 400 #Make a variable for velocity so it can be changed later.
+var velocity = Vector2.ZERO
 
 #The following function checks inputs then changes player velocity on physics updates.
 func _physics_process(delta): #Anything in this function is done on physics updates.
-	if Input.is_action_pressed("ui_left"):
-		velocity.x = -4
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x = 4
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector
 	else:
-		velocity.x = 0
-	if Input.is_action_pressed("ui_up"):
-		velocity.y = -4
-	elif Input.is_action_pressed("ui_down"):
-		velocity.y = 4
-	else:
-		velocity.y = 0
+		velocity = Vector2.ZERO
 	move_and_collide(velocity)
-	#There's probably a better way to move the player without this yanderedev-level spaghetti code but I can't be bothered to find out what it is.
