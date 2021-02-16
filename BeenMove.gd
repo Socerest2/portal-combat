f51f4515559e9da68462d16caf83f9749ebd3a0b
Collig0 <43286422+Collig0@users.noreply.github.com>
2021-02-16 16:20:06 -0500

    Fix velocities (again)
    just following the tutorial

--- BeenMove.gd
+++ BeenMove.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
 
-const ACCELERATION = 400
-const MAX_SPEED = 150
-const FRICTION = 400 #Make a variable for velocity so it can be changed later.
+const ACCELERATION = 50
+const MAX_SPEED = 250
+const FRICTION = 35 #Make a variable for velocity so it can be changed later.
 var velocity = Vector2.ZERO
 
 #The following function checks inputs then changes player velocity on physics updates.
@@ -11,9 +11,11 @@ func _physics_process(delta): #Anything in this function is done on physics upda
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity += input_vector * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED * delta)
 	else:
-		velocity = Vector2.ZERO
-	move_and_collide(velocity * delta * MAX_SPEED)
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	move_and_collide(velocity)
